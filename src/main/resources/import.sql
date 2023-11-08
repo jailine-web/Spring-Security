141d89cb88f303d8e8007827d6a65172f5f8f6ef
Jailine <jay.santana964@gmail.com>
2023-11-08 18:41:32 -0300

    Inserindo controles de acesso via regra e rotas

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '123456');
-INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '123456');
+INSERT INTO tb_user (name, email, password) VALUES ('Alex', 'alex@gmail.com', '$2a$12$/fR4B44CuHtbJuAeRaD29uRS.YdAztRZC3B2va3ox9hkq7cK69MUO');
+INSERT INTO tb_user (name, email, password) VALUES ('Maria', 'maria@gmail.com', '$2a$12$/fR4B44CuHtbJuAeRaD29uRS.YdAztRZC3B2va3ox9hkq7cK69MUO');
 
 INSERT INTO tb_role (authority) VALUES ('ROLE_OPERATOR');
 INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
